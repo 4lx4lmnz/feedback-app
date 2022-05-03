f8f783491f44563310df49039dbc13ffd3dec42d
Alex Almanza <44lmanza@gmail.com>
2022-05-03 09:21:04 -0700

    Add feedback add

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { v4 as uuidv4 } from 'uuid';
 import { useState } from 'react';
 import Header from './components/Header';
 import FeedbackList from './components/FeedbackList';
@@ -9,6 +10,14 @@ import FeedbackData from './data/FeedbackData';
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData);
 
+  const addFeedaback = (newFeedback) => {
+    // give a unique id
+    newFeedback.id = uuidv4();
+    // cant push onto feedback, immutable, must use spread to recreate array
+    setFeedback([newFeedback, ...feedback]);
+    console.log('New feedback: ', newFeedback);
+  };
+
   const deleteFeedback = (id) => {
     if (window.confirm('Are you sure you want to delete')) {
       setFeedback(feedback.filter((item) => item.id !== id));
@@ -19,7 +28,7 @@ function App() {
     <>
       <Header />
       <div className='container'>
-        <FeedbackForm />
+        <FeedbackForm handleAdd={addFeedaback} />
         <FeedbackStats feedback={feedback} />
         <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
       </div>
--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -3,7 +3,7 @@ import Card from './shared/Card';
 import Button from './shared/Button';
 import RatingSelect from './RatingSelect';
 
-function FeedbackForm() {
+function FeedbackForm({ handleAdd }) {
   const [text, setText] = useState('');
   const [rating, setRating] = useState(10);
   const [btnDisabled, setBtnDisabled] = useState(true);
@@ -26,9 +26,24 @@ function FeedbackForm() {
     setText(e.target.value);
   };
 
+  const handleSubmit = (e) => {
+    // normally tries to submit to file, overide
+    e.preventDefault();
+    // there are client side ways to override form checking, so add more
+    if (text.trim().length > 10) {
+      //short hand for creating a new object
+      const newFeedback = {
+        text,
+        rating,
+      };
+      handleAdd(newFeedback);
+      setText('');
+    }
+  };
+
   return (
     <Card>
-      <form>
+      <form onSubmit={handleSubmit}>
         <h2>How would you rate our service?</h2>
         <RatingSelect select={(rating) => setRating(rating)} />
         <div className='input-group'>
