b3b83e42b900c7cc386615a3a365fd11ce26fb6e
Alex Almanza <44lmanza@gmail.com>
2022-05-05 11:24:02 -0700

    Add redirect examples

--- src/App.js
+++ src/App.js
@@ -49,7 +49,7 @@ function App() {
             }
           />
           <Route path='/about' element={<AboutPage />} />
-          <Route path='/post/:id' element={<Post />} />
+          <Route path='/post/*' element={<Post />} />
         </Routes>
         <AboutIconLink />
       </div>
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,10 +1,25 @@
-import { useParams } from 'react-router-dom';
+import { Navigate, useNavigate, Routes, Route } from 'react-router-dom';
 function Post() {
-  const params = useParams();
+  const status = 200;
+
+  const navigate = useNavigate();
+
+  const onClick = () => {
+    console.log('Hello');
+    navigate('/about');
+  };
+
+  if (status === 404) {
+    return <Navigate to='/notfound' />;
+  }
 
   return (
     <div>
-      <h1>This is post {params.id}</h1>
+      <h1>This is post</h1>
+      <button onClick={onClick}>Click</button>
+      <Routes>
+        <Route path='/show' element={<h1>Hello World</h1>} />
+      </Routes>
     </div>
   );
 }
